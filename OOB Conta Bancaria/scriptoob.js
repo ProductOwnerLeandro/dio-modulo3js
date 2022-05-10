4e3aad47e45529dde6aaa25fe37039600ff170d5
productownerleandro <leandro@bruteforce.com.br>
2022-05-09 22:25:36 -0300

    Update Orientação a Objetos

---
+++ OOB Conta Bancaria/scriptoob.js
@@ -0,0 +1,76 @@
+class ConataBancaria{
+    constructor(agencia, numero, tipo, saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+    
+    get saldo(){
+        return this._saldo;
+    }
+
+    set saldo(valor){
+        this._saldo = valor; 
+    }
+
+    sacar(valor){
+
+       if  (valor > this._saldo) {
+           return 'Operação negada';
+       }
+       this._saldo = this._saldo - valor;
+    
+       return this._saldo;
+    }
+
+    depositar(valor){
+       this._saldo = this._saldo + valor;
+       return this._saldo;
+    }
+
+}
+
+/////////////////- Contas -/////////////////
+class contaCorrente extends ConataBancaria{
+    constructor(agencia, numero, cartaoCredito) {
+        super(agencia, numero);
+        this.tipo = 'corrente';
+        this._cartaoCredito = cartaoCredito;
+        
+    }
+
+    get cartaoCredito(){
+        return this._cartaoCredito;
+    }
+
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor;
+    }
+
+}
+
+///////////////////////////////////////////
+class contaPoupanca extends ConataBancaria{
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'poupanca';        
+    }
+
+}
+///////////////////////////////////////////
+class contaUniversitaria extends ConataBancaria{
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'universitaria';       
+    }
+
+    sacar(valor){
+        if(valo > 500){
+            return 'Operação negada!'
+        }
+        this._saldo = this._saldo - valor;
+    }
+
+}
+
